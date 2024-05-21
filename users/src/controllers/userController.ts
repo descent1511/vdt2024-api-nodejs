2224954d725d634964cd34935b6805c94795a89f
descent1511 <natibatian@gmail.com>
2024-05-21 21:06:36 +0300

    feat: integrate AWS S3 for user avatar uploads

--- users/src/controllers/userController.ts
+++ users/src/controllers/userController.ts
@@ -1,8 +1,27 @@
 import { Op } from 'sequelize';
 import { User } from '../models/user';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import multer from 'multer';
 import { sendSuccessResponse, sendCreatedResponse, sendErrorResponse, sendNotFoundResponse } from '../utils/responseHandler';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage }).single('avatar');
 
 export default class UserController {
+    private s3Client: S3Client;
+
+    constructor() {
+        this.s3Client = new S3Client({
+            region: process.env.BUCKET_REGION,
+            credentials: {
+                accessKeyId: process.env.ACCESS_KEY!,
+                secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+            },
+        });
+    }
 
     createUser = async (req: any, res: any) => {
         try {
@@ -66,7 +85,6 @@ export default class UserController {
     searchUserByName = async (req: any, res: any) => {
         try {
             const { search } = req.query;
-            console.log(search)
             if (!search) {
                 return sendErrorResponse(res, { message: 'Search query is required' }, 400);
             }
@@ -77,11 +95,45 @@ export default class UserController {
                     }
                 }
             });
-            
             sendSuccessResponse(res, users, 'Users retrieved successfully');
-           
         } catch (error) {
             sendErrorResponse(res, error);
         }
+    };
+
+    uploadAvatar = async (req: any, res: any) => {
+        upload(req, res, async (err: any) => {
+            if (err) {
+                return sendErrorResponse(res, err);
+            }
+
+            if (!req.file) {
+                return sendErrorResponse(res, { message: 'No file uploaded' }, 400);
+            }
+
+            try {
+                const user = await User.findByPk(req.params.id);
+                if (!user) {
+                    return sendNotFoundResponse(res);
+                }
+
+                const params = {
+                    Bucket: process.env.BUCKET_NAME!,
+                    Key: `avatars/${req.file.originalname}-${Date.now()}`,
+                    Body: req.file.buffer,
+                    ContentType: req.file.mimetype,
+                };
+
+                const command = new PutObjectCommand(params);
+                await this.s3Client.send(command);
+
+                const avatarUrl = `https://${process.env.BUCKET_NAME}.s3.${process.env.BUCKET_REGION}.amazonaws.com/${params.Key}`;
+
+                await user.update({ imgUrl:avatarUrl });
+                sendSuccessResponse(res, { avatarUrl }, 'Avatar uploaded successfully');
+            } catch (error) {
+                sendErrorResponse(res, error);
+            }
+        });
     };
 }
